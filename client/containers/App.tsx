727a0f581b9df177f3152f1006756b44ad2e2f9f
Joel Mueller <joel.mueller@gmail.com>
2016-02-11 23:54:01 -0600

    Initial commit

---
+++ client/containers/App.tsx
@@ -0,0 +1,38 @@
+/// <reference path="../../typings/tsd.d.ts" />
+
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as React from 'react';
+import * as Immutable from 'immutable';
+
+import Header from '../components/Header';
+import MainSection from '../components/MainSection';
+import * as TodoActions from '../actions/todos';
+import { TodoList } from '../models/todos';
+
+interface AppProps {
+  todos?: TodoList;
+  dispatch?: Redux.Dispatch;
+}
+
+class App extends React.Component<AppProps, any> {
+  render() {
+    const { todos, dispatch } = this.props;
+    const actions = bindActionCreators(TodoActions, dispatch);
+
+    return (
+      <div className="todoapp">
+        <Header addTodo={actions.addTodo} />
+        <MainSection
+          todos={todos}
+          actions={actions}/>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  todos: state.todos
+});
+
+export default connect(mapStateToProps)(App);
---
+++ typings/tsd.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="classnames/classnames.d.ts" />
+/// <reference path="react-redux/react-redux.d.ts" />
+/// <reference path="react/react.d.ts" />
+/// <reference path="redux-actions/redux-actions.d.ts" />
+/// <reference path="redux/redux.d.ts" />
+/// <reference path="immutable/immutable-overrides.d.ts" />
