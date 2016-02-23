fd8dfe7701f2de334eda121a2d0d4a768e111a04
Joel Mueller <joel.mueller@gmail.com>
2016-02-22 23:05:06 -0600

    Setting up github pages

--- client/constants/TodoFilters.ts
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-export type FilterType = 'show_all' | 'show_completed' | 'show_active';
-
-export const ShowAll: FilterType = 'show_all';
-export const ShowCompleted: FilterType = 'show_completed';
-export const ShowActive: FilterType = 'show_active';
